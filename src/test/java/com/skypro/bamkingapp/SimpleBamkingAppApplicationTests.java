09967423e6e14b2e8203c571f68d64cc77466561
дом <amir/shakirov.2015@mail.ru>
2023-03-01 09:43:27 +0400

    initial commit

---
+++ src/test/java/com/skypro/bamkingapp/SimpleBamkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.bamkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBamkingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
